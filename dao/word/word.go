d842e68b3889c0c81e030ed1e51a8742d0300e95
dteer <dteer@qq.com>
2022-11-01 00:08:18 +0800

    数据入库

--- dao/word/word.go
+++ dao/word/word.go
@@ -3,10 +3,12 @@ package word
 import "time"
 
 type Word struct {
-	ID          uint      `gorm:"primarykey"`
-	Title       string    `gorm:"title"`
-	Description string    `gorm:"description"`
-	StartTime   time.Time `gorm:"start_time"`
-	CreateTime  time.Time `gorm:"create_time"`
-	UpdatedTime time.Time `gorm:"updated_time"`
+	ID              uint      `gorm:"primarykey"`
+	Title           string    `gorm:"title"`
+	PhoneticStymbol string    `gorm:"phonetic_stymbol"`
+	Description     string    `gorm:"description"`
+	StartTime       time.Time `gorm:"start_time"`
+	ShowTime        int64     `gorm:"show_time"`
+	CreateTime      time.Time `gorm:"create_time"`
+	UpdatedTime     time.Time `gorm:"updated_time"`
 }
