bb87cdfdfef53ea03ecf9f85526bc07501d246a6
dteer <dteer@qq.com>
2022-11-01 23:59:30 +0800

    调节显示方式

--- service/run.go
+++ service/run.go
@@ -52,13 +52,14 @@ func getOldWord(count int, tody int) (words []word.Word) {
 func popWord(w word.Word) {
 	notification := toast.Notification{
 		AppID:   "Microsoft.Windows.Shell.RunDialog",
-		Title:   w.Title + "  [" + w.PhoneticStymbol + "]",
-		Message: w.Description,
+		Title:   w.Title,
+		Message: "[" + w.PhoneticStymbol + "]\n\n" + w.Description,
 		Icon:    "C:\\path\\to\\your\\logo.png", // 文件必须存在
-		Actions: []toast.Action{
-			{"protocol", "按钮1", "https://www.google.com/"},
-			{"protocol", "按钮2", "https://github.com/"},
-		},
+		// Actions: []toast.Action{
+		// 	{"protocol", "按钮1", "https://www.google.com/"},
+		// 	{"protocol", "按钮2", "https://github.com/"},
+		// },
+		Duration: "long",
 	}
 	err := notification.Push()
 	if err != nil {
